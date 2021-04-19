26608d09dd2d378068d99e7a4cf76fcac05896c4
Daniel Coles <me@danielcoles.co.uk>
2021-04-19 22:55:39 +0100

    retabulated queries

--- sql.go
+++ sql.go
@@ -1,43 +1,49 @@
 package main
 
 const sqlGetResults = `
-		SELECT
-				id,
-				name,
-				CASE 	
-						WHEN ts_headline(classification, q) LIKE '%<b>%' 					
-								THEN CONCAT('Classification: ', ts_headline(classification, q))
-						WHEN ts_headline(name, q) LIKE '%<b>%' 								
-								THEN CONCAT('Name: ', ts_headline(name, q))
-						WHEN ts_headline(CAST(id AS TEXT), q) LIKE '%<b>%' 					
-								THEN CONCAT('Pokedex: ', ts_headline(CAST(id AS TEXT), q))
-						WHEN ts_headline(array_to_string(types, ', '), q) LIKE '%<b>%'		
-								THEN CONCAT('Types: ', ts_headline(array_to_string(types, ', '), q))
-						WHEN ts_headline(array_to_string(abilities, ', '), q) LIKE '%<b>%' 	
-								THEN CONCAT('Abilities: ', ts_headline(array_to_string(abilities, ', '), q))
-				ELSE '' END AS snippet
-		FROM (
-				SELECT
-					id, name, types, classification, abilities, ts_rank(tsv, q) as rank, q
-				FROM
-					pokemon, plainto_tsquery($1) q
-				WHERE
-					tsv @@ q
-				ORDER BY
-					rank DESC
-			LIMIT 10
-		) AS a
-		ORDER BY
-			rank DESC;
+SELECT
+	id,
+	name,
+	CASE 	
+		WHEN ts_headline(classification, query) LIKE '%<b>%' 					
+				THEN CONCAT('Classification: ', ts_headline(classification, query))
+		WHEN ts_headline(name, query) LIKE '%<b>%' 								
+				THEN CONCAT('Name: ', ts_headline(name, query))
+		WHEN ts_headline(CAST(id AS TEXT), query) LIKE '%<b>%' 					
+				THEN CONCAT('Pokedex: ', ts_headline(CAST(id AS TEXT), query))
+		WHEN ts_headline(array_to_string(types, ', '), query) LIKE '%<b>%'		
+				THEN CONCAT('Types: ', ts_headline(array_to_string(types, ', '), query))
+		WHEN ts_headline(array_to_string(abilities, ', '), query) LIKE '%<b>%' 	
+				THEN CONCAT('Abilities: ', ts_headline(array_to_string(abilities, ', '), query))
+	ELSE '' END AS snippet
+FROM (
+	SELECT
+		id, 
+		name, 
+		types, 
+		classification, 
+		abilities, 
+		ts_rank(tsv, query) as rank, 
+		query
+	FROM
+		pokemon, plainto_tsquery($1) query
+	WHERE
+		tsv @@ query
+	ORDER BY
+		rank DESC
+	LIMIT 10
+) AS results
+ORDER BY
+	rank DESC;
 `
 
 const sqlGetDetails = `
-		SELECT
-				id,
-				name,
-				types,
-				classification,
-				abilities
-		FROM pokemon
-		WHERE id=$1
+SELECT
+	id,
+	name,
+	types,
+	classification,
+	abilities
+FROM pokemon
+WHERE id=$1
 `
